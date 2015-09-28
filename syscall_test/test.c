9d40b606ec164e301aedd4bd5f0121c1bbcda75f
Chen-Yu Yang <cy2417@columbia.edu>
2015-09-27 22:40:34 -0400

    Now it prints tree-like processes in DFS way

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -3,14 +3,27 @@
 #include <unistd.h>
 #include "prinfo.h"
 
+#define N 100
+
 int main()
 {
-	struct prinfo p;
-	int nr = 10, pc;
+	struct prinfo p[N], q;
+	int nr = N, pc, i, j, indents[N] = {0};
 
-	printf("run test\n");
-	pc = syscall(223, &p, &nr);
-	printf("nr after system call: %d\n", nr);
-	printf("total processes count: %d\n", pc);
+	pc = syscall(223, p, &nr);
+
+	for (i = 0; i < nr && i < pc; i++) {
+		q = p[i];
+		j = 0;
+		while (indents[j] != q.pid && j < 5) {
+			printf("\t");
+			j++;
+		}
+		indents[j] = q.next_sibling_pid;
+		indents[j+1] = q.first_child_pid;
+		printf("%s,%d,%ld,%d,%d,%d,%ld\n", q.comm, q.pid, q.state,
+		       q.parent_pid, q.first_child_pid, q.next_sibling_pid,
+		       q.uid);
+	}
 	return 0;
 }
