f7c4a966cb915ec984c827e0d3595a6908720061
Chen-Yu Yang <cy2417@columbia.edu>
2015-09-28 09:37:37 -0400

    Refactor and rename variables

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -7,25 +7,22 @@
 
 int main(void)
 {
-	struct prinfo p[N], q;
+	struct prinfo ps[N], p;
 	int nr = N, pc, i, j, indents[N] = {0};
 	/* indents store the next pid has such indent level.
 	   If indents[1] = 10, it means process 10 has indent level 1 */
 
-	pc = syscall(223, p, &nr);
+	pc = syscall(223, ps, &nr);
 
 	for (i = 0; i < nr && i < pc; i++) {
-		q = p[i];
-		j = 0;
-		while (indents[j] != q.pid) {
+		p = ps[i];
+		for (j = 0; indents[j] != p.pid; j++)
 			printf("\t");
-			j++;
-		}
-		indents[j] = q.next_sibling_pid;
-		indents[j+1] = q.first_child_pid;
-		printf("%s,%d,%ld,%d,%d,%d,%ld\n", q.comm, q.pid, q.state,
-		       q.parent_pid, q.first_child_pid, q.next_sibling_pid,
-		       q.uid);
+		indents[j] = p.next_sibling_pid;
+		indents[j+1] = p.first_child_pid;
+		printf("%s,%d,%ld,%d,%d,%d,%ld\n", p.comm, p.pid, p.state,
+		       p.parent_pid, p.first_child_pid, p.next_sibling_pid,
+		       p.uid);
 	}
 	return 0;
 }
